92b9444085faf984e4e514d5ffa26e5a5c7c081a
cpbenite <cpbenite@ucsd.edu>
2019-05-29 16:18:34 -0700

    Replace express-session with cookie-session

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const flash = require('connect-flash');
-const session = require('express-session');
+const session = require('cookie-session');
 
 const app = express();
 
